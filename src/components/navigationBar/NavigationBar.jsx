339c3cab93e56b05325fc4addbeb7811928f5783
Mariusz Przodała <mariuszprzodala@Mariuszs-MacBook-Pro.local>
2022-12-07 19:18:11 +0100

    remove elements

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import routes from "../../constants/routes";
 import useStoreRoute from "../../hooks/useStoreRoute";
-import NavigationItem from "./NavigationItem";
 import NavigationButton from "./NavigationButton";
-
-const drawerWidth = 240;
 
 const navItems = [
     routes.home,
@@ -23,55 +15,20 @@ const navItems = [
 ];
 
 function NavigationBar() {
-    const { pathname } = useStoreRoute(); // save current route in store
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
-
-    const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-            <Typography variant="h6" sx={{ my: 2 }}>
-                Star Wars API
-            </Typography>
-            <Divider />
-            <List>
-                {navItems.map(({ path, label }) => (
-                    <NavigationItem
-                        key={path}
-                        path={path}
-                        label={label}
-                        isActive={path === pathname}
-                    />
-                ))}
-            </List>
-        </Box>
-    );
-
+    const { pathname } = useStoreRoute();
 
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box>
             <AppBar component="nav">
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ display: { sm: 'none' } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography
                         variant="h6"
                         component="div"
-                        align="left"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                        marginRight={2}
                     >
                         Star Wars API
                     </Typography>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                    <Box>
                         {navItems.map(({ path, label }) => (
                             <NavigationButton
                                 key={path}
@@ -83,22 +40,6 @@ function NavigationBar() {
                     </Box>
                 </Toolbar>
             </AppBar>
-            <Box component="nav">
-                <Drawer
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: { xs: 'block', sm: 'none' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-            </Box>
         </Box>
     );
 }
